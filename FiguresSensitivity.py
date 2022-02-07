5931634775e5da35e597b94d0ff4724106e214eb
Jaakko Ahola <jaakko.ahola@fmi.fi>
2022-02-07 11:47:53 +0200

    modified:   FiguresSensitivity.py 	- total make over (divide & conquer)

--- FiguresSensitivity.py
+++ FiguresSensitivity.py
@@ -23,7 +23,7 @@ from PlotTweak import PlotTweak
 
 sys.path.append("../LES-emulator-02postpros")
 from plotEMUL import ManuscriptFigures
-
+from itertools import repeat
 
 class FiguresSensitivity(ManuscriptFigures):
 
@@ -39,41 +39,103 @@ class FiguresSensitivity(ManuscriptFigures):
             self.sensitivity_sets[key] = ManuscriptFigures(dict_of_locationsFiles[key])
 
         self.filter_names = list(self.dict_of_locationsFiles)
-        self.rmse = {}
 
-        for ind, trainingSet in enumerate(self.trainingSetList):
-            self.rmse[trainingSet] = {}
-            for predictor in self.predictorShortNames:
-                self.rmse[trainingSet][self.predictorClearNames[predictor]] = numpy.zeros(len(self.filter_names))
+        self.stats_names = ["rmse", "rSquared"]
+        self.stats_proper_names = dict(zip(self.stats_names, ["RMSE", PlotTweak.getLatexLabel('R^2')]))
+        self.sens_stats = dict(zip(self.stats_names, repeat(None)))
 
 
-    def collect_sensitivity_stats(self):
-        for ind, trainingSet in enumerate(self.trainingSetList):
-            for predictor in self.predictorShortNames:
-                self.rmse[trainingSet][self.predictorClearNames[predictor]] = [ self.sensitivity_sets[key].statsCollection[trainingSet].loc[predictor]["rmse"] for key in self.dict_of_locationsFiles]
+        for stat_name in self.stats_names:
+            self.sens_stats[stat_name] = {}
+            for ind, trainingSet in enumerate(self.trainingSetList):
+                self.sens_stats[stat_name][trainingSet] = {}
+                for predictor in self.predictorShortNames:
+                    self.sens_stats[stat_name][trainingSet][self.predictorClearNames[predictor]] = numpy.zeros(len(self.filter_names))
+
+        self._collect_stats()
+
+    def _collect_stats(self):
+        for stat_name in self.stats_names:
+            for ind, trainingSet in enumerate(self.trainingSetList):
+                for predictor in self.predictorShortNames:
+                    self.sens_stats[stat_name][trainingSet][self.predictorClearNames[predictor]] = [ self.sensitivity_sets[key].statsCollection[trainingSet].loc[predictor][stat_name] for key in self.dict_of_locationsFiles]
 
 
-    def figure_sensitivity(self):
-        name = "figure_sensitivity"
+    def figure_sensitivity_rmse(self):
+        self._figure_sensitivity("rmse",
+                                        [0, 0.12, 0.02],
+                                        2)
+
+
+
+    def figure_sensitivity_rSquared(self):
+        self._figure_sensitivity("rSquared",
+                                        [0, 1, 0.2],
+                                        1)
+
+
+    def _figure_sensitivity(self, stat_name, ytick_specs, float_decimals):
+        name = "figure_sensitivity_" + stat_name
         self.figures[name] = Figure(self.figureFolder,name, figsize = [self.figureWidth, 4],
                                     ncols=2, nrows=2,
                                     hspace=0.08, wspace=0.12,
                                     bottom=0.11, top=0.93,
+                                    left = 0.15, right=0.97
                                     )
 
         fig = self.figures[name]
 
+        ystart = ytick_specs[0]
+        yend = ytick_specs[1]
+        yinterval = ytick_specs[2]
+
+        yticks = numpy.arange(ystart, yend + yinterval, yinterval)
+        ytickLabels = [f"{t:.{float_decimals}f}" for t in yticks]
+        yshowList = Data.cycleBoolean(len(yticks))
+        yshowList[-1]=False
 
         for ind, trainingSet in enumerate(self.trainingSetList):
+
             current_axes = fig.getAxes(ind)
-            data = self.rmse[trainingSet]
+            data = self.sens_stats[stat_name][trainingSet]
             index = ["NA", "Soft", "Hard"]
             df = pandas.DataFrame(data, index=index)
-            df.plot.bar(rot=0, ax=current_axes, color = self.predictorColors.values())
+            df.plot.bar(rot=0,
+                        ax=current_axes,
+                        color=self.predictorColors.values(),
+                        legend=False)
 
+            current_axes.set_yticks(yticks)
+            current_axes.set_yticklabels(ytickLabels)
+            PlotTweak.hideLabels(current_axes.yaxis, yshowList)
 
+            PlotTweak.setAnnotation(current_axes, self.annotationCollection[trainingSet],
+                                    xPosition=PlotTweak.getXPosition(current_axes, 0.20),
+                                    yPosition=PlotTweak.getYPosition(current_axes, 0.90))
 
+            if ind in [0,1]:
+                PlotTweak.hideXTickLabels(current_axes)
+            if ind in [1,3]:
+                PlotTweak.hideYTickLabels(current_axes)
 
+            if ind == 1:
+                collectionOfLabelsColors = dict(zip(list(self.predictorClearNames.values()), list(self.predictorColors.values())))
+                legendLabelColors = PlotTweak.getPatches(collectionOfLabelsColors)
+
+                artist = current_axes.legend(handles=legendLabelColors,
+                                              loc=(-0.6, 1.05),
+                                              frameon=True,
+                                              framealpha=1.0,
+                                              ncol=3)
+
+                current_axes.add_artist(artist)
+
+            if ind == 0:
+                current_axes.text(PlotTweak.getXPosition(current_axes, -0.30),
+                                  PlotTweak.getYPosition(current_axes, -0.05),
+                                  self.stats_proper_names[stat_name],
+                                  size=8,
+                                  rotation=90)
 
 def main():
     try:
@@ -85,10 +147,9 @@ def main():
                                   }
 
     fs = FiguresSensitivity(dict_of_locationsFiles)
-    fs.collect_sensitivity_stats()
-    print(fs.rmse)
 
-    fs.figure_sensitivity()
+    fs.figure_sensitivity_rmse()
+    fs.figure_sensitivity_rSquared()
 
     fs.finalise()
 
